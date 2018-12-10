3353d415bb1f37baad3765ec907857c441189b6b
Jenit Vaghasiya <JenitV@sfwltd.co.uk>
2018-12-10 17:43:16 +0530

    initial update

---
+++ App_Code/SSRSReport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+
+/// <summary>
+/// Summary description for SSRSReport
+/// </summary>
+public class SSRSReport
+{
+    
+
+	public SSRSReport()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public bool CreateReport(String Connectionstring,string StoreProcedureName ,SqlParameter[] Parameter,ref  Microsoft.Reporting.WebForms.ReportViewer ReportViewer,string ReportDataSource)
+    {
+        bool reportbind = false;
+        using (SqlConnection con = new SqlConnection(Connectionstring))
+        {
+            SqlCommand com = new SqlCommand();
+            com.Connection = con;
+            com.CommandType = CommandType.StoredProcedure;
+            com.CommandText = StoreProcedureName;
+            com.Parameters.AddRange(Parameter);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            da.Fill(ds);
+            ReportDataSource datasource = new ReportDataSource(ReportDataSource, ds.Tables[0]); 
+            if ( ds.Tables[0].Rows.Count > 0)
+            {
+                ReportViewer.LocalReport.DataSources.Clear();
+                ReportViewer.LocalReport.DataSources.Add(datasource);
+                reportbind = true;
+            }                       
+        }
+       return  reportbind;
+    }
+
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    string Connectionstring = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            
+        }
+       
+    }
+
+    private void BindReport(string p, int p_2)
+    {
+        SSRSReport report = new SSRSReport();
+        SqlParameter[] sqlParams = new SqlParameter[] {
+         new SqlParameter("@Add_Source_Id", 25) ,
+         new SqlParameter("@Add_Source_Type", "Employee")
+      };
+
+        string ReportDataSource = "DataSet1";
+        bool bind = report.CreateReport(Connectionstring, "Get_Address_sp", sqlParams, ref ReportViewer1, ReportDataSource);
+        if (bind)
+        {
+            ReportViewer1.Visible = true;
+        }
+    }
+
+    protected void btndisplay_Click(object sender, EventArgs e)
+    {
+        BindReport("Employee", 14);
+    }
+}
